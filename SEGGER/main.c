bcf21e0bef2a85726394fa32a71ee63c0c3b0ee1
Ronakkumar Sharma <51168291+sharmaronak79@users.noreply.github.com>
2021-05-03 21:17:43 +0530

    Update main.c

--- SEGGER/main.c
+++ SEGGER/main.c
@@ -5,6 +5,7 @@
   * @version V1.0
   * @date    01-December-2013
   * @brief   Default main function.
+
   ******************************************************************************
 */
 #include "FreeRTOS.h"
